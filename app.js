8d1323811d1e31f137db9313b90037c082d1bf29
Rahul Panchal <rm5901960@gmail.com>
2023-09-22 23:45:26 +0530

    update

--- app.js
+++ app.js
@@ -11,5 +11,13 @@ app.use(express.static(path.join(__dirname,'..','public')));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'));
 })
+app.get('/*',(req,res)=>{
+    res.status(400).json({
+        "error":"The page does not exists"
+    })
+})
+app.get('/profile',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','profile.html'));
+})
 
 module.exports= app;
